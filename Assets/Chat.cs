4a7e304b9e6537174cfc09e9e7eaad2f15f7b017
krzys-h <krzys_h@interia.pl>
2018-07-05 15:59:27 +0200

    Chat update

---
+++ Assets/Chat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Chat : NetworkBehaviour
+{
+    public static Chat instance;
+    private string lastMessage = "";
+
+    void Start()
+    {
+        instance = this;
+    }
+    
+    [ClientRpc]
+    public void RpcDisplayChat(string message)
+    {
+        Debug.Log("Chat: "+message);
+        lastMessage = message;
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(lastMessage);
+    }
+}
--- Assets/Walking.cs
+++ Assets/Walking.cs
@@ -2,47 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Networking;
 
-public class Walking : MonoBehaviour {
+public class Walking : NetworkBehaviour {
     public AudioClip oof;
     public Transform target;
 
 	// Use this for initialization
 	void Start () {
-		
+        if (!isLocalPlayer)
+        {
+            GetComponentInChildren<Camera>().enabled = false;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float turbo = 1.0f;
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (isLocalPlayer)
         {
-            turbo = 1.5f;
-        }
-
-		if (Input.GetKey(KeyCode.W))
-        {
-            GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, 30) * Time.deltaTime * turbo, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, -30) * Time.deltaTime * turbo, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 0, -15) * Time.deltaTime * turbo, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 0, 15) * Time.deltaTime * turbo, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, -25, 0) * Time.deltaTime * turbo, ForceMode.VelocityChange);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 25, 0) * Time.deltaTime * turbo, ForceMode.VelocityChange);
+            if (Input.GetKey(KeyCode.W))
+            {
+                GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, 30) * Time.deltaTime, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, -30) * Time.deltaTime, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 0, -15) * Time.deltaTime, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey(KeyCode.Q))
+            {
+                GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 0, 15) * Time.deltaTime, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, -25, 0) * Time.deltaTime, ForceMode.VelocityChange);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 25, 0) * Time.deltaTime, ForceMode.VelocityChange);
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+                CmdSendChat("test");
         }
 
         /*RaycastHit hit;
@@ -52,7 +55,7 @@ public class Walking : MonoBehaviour {
             transform.up = Vector3.MoveTowards(transform.up, hit.normal, Time.deltaTime);
         }*/
 
-        GetComponent<AudioSource>().pitch = Mathf.Clamp(GetComponent<Rigidbody>().velocity.magnitude / 15.0f, 0.6f, turbo > 1.0f ? 99.0f : 1.0f);
+        GetComponent<AudioSource>().pitch = Mathf.Clamp(GetComponent<Rigidbody>().velocity.magnitude / 15.0f, 0.6f, 1.0f);
 
         //GetComponent<NavMeshAgent>().SetDestination(target.position);
         /*GetComponent<Rigidbody>().AddForce((target.position - transform.position).normalized * Time.deltaTime * 1000.0f, ForceMode.Impulse);
@@ -62,5 +65,11 @@ public class Walking : MonoBehaviour {
     void OnCollisionEnter(Collision collision)
     {
         AudioSource.PlayClipAtPoint(oof, transform.position);
+    }
+
+    [Command]
+    public void CmdSendChat(string message)
+    {
+        Chat.instance.RpcDisplayChat(message);
     }
 }
