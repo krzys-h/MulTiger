b442fa379c0816908df8599cb1ba4786a6ff4941
krzys-h <krzys_h@interia.pl>
2018-07-06 16:36:36 +0200

    Music and improvements

--- Assets/Walking.cs
+++ Assets/Walking.cs
@@ -9,6 +9,11 @@ public class Walking : NetworkBehaviour {
     public Transform target;
     [SyncVar]
     public string nick = "";
+    [SyncVar(hook = "OnSongChanged")]
+    public int song;
+    [SyncVar]
+    public float hp = 666;
+    public AudioClip[] songs;
 
 	// Use this for initialization
 	void Start () {
@@ -16,41 +21,46 @@ public class Walking : NetworkBehaviour {
         {
             GetComponentInChildren<Camera>().enabled = false;
             GetComponentInChildren<AudioListener>().enabled = false;
+            transform.Find("[CameraRig]").gameObject.SetActive(false);
         }
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        Vector2 joystick = SteamVR_Controller.Input(1).GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis2) + SteamVR_Controller.Input(2).GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis2);
+        Vector2 touchpad = SteamVR_Controller.Input(1).GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0) + SteamVR_Controller.Input(2).GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+        if (joystick.magnitude < 0.25f)
+            joystick = Vector3.zero;
+        if (touchpad.magnitude < 0.25f)
+            touchpad = Vector3.zero;
+        float forward = joystick.y;
+        float turn = joystick.x;
+        float roll = touchpad.x;
+        if (Input.GetKey(KeyCode.W))
+            forward += 1.0f;
+        if (Input.GetKey(KeyCode.S))
+            forward -= 1.0f;
+        if (Input.GetKey(KeyCode.A))
+            turn -= 1.0f;
+        if (Input.GetKey(KeyCode.D))
+            turn += 1.0f;
+        if (Input.GetKey(KeyCode.Q))
+            roll += 1.0f;
+        if (Input.GetKey(KeyCode.E))
+            roll -= 1.0f;
+        if (Input.GetKey(KeyCode.LeftShift))
+            forward *= 3.0f;
         if (isLocalPlayer)
         {
-            if (Input.GetKey(KeyCode.W))
-            {
-                GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, 30) * Time.deltaTime, ForceMode.VelocityChange);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, -30) * Time.deltaTime, ForceMode.VelocityChange);
-            }
-            if (Input.GetKey(KeyCode.E))
-            {
-                GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 0, -15) * Time.deltaTime, ForceMode.VelocityChange);
-            }
-            if (Input.GetKey(KeyCode.Q))
-            {
-                GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 0, 15) * Time.deltaTime, ForceMode.VelocityChange);
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, -25, 0) * Time.deltaTime, ForceMode.VelocityChange);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 25, 0) * Time.deltaTime, ForceMode.VelocityChange);
-            }
+            //Debug.Log(forward + " " + turn + " " + roll);
+            GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, 50 * forward) * Time.deltaTime, ForceMode.VelocityChange);
+            GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 0, 15 * roll) * Time.deltaTime, ForceMode.VelocityChange);
+            GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 25 * turn, 0) * Time.deltaTime, ForceMode.VelocityChange);
             if (Input.GetKeyDown(KeyCode.Space))
                 CmdSendChat("test");
         }
-        GetComponentInChildren<TextMesh>().text = nick;
+        GetComponentInChildren<TextMesh>().text = nick+" [HP: "+hp.ToString("N2")+"]";
 
         /*RaycastHit hit;
         if(Physics.Raycast(transform.position, Vector3.down, out hit))
@@ -69,6 +79,11 @@ public class Walking : NetworkBehaviour {
     void OnCollisionEnter(Collision collision)
     {
         AudioSource.PlayClipAtPoint(oof, transform.position);
+        if (collision.gameObject.GetComponent<Walking>() != null)
+        {
+            if (collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude > this.GetComponent<Rigidbody>().velocity.magnitude)
+                hp -= Mathf.Pow(Mathf.Abs(collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude - this.GetComponent<Rigidbody>().velocity.magnitude), 0.6f) * 25.0f;
+        }
     }
 
     [Command]
@@ -81,13 +96,26 @@ public class Walking : NetworkBehaviour {
 
     void OnGUI()
     {
-        if (isLocalPlayer && nick == "")
+        if (isLocalPlayer)
         {
-            editName = GUILayout.TextField(editName);
-            if (GUILayout.Button("Set name"))
+            if (nick == "")
             {
-                CmdSetNick(editName);
+                editName = GUILayout.TextField(editName);
+                if (GUILayout.Button("Set name"))
+                {
+                    CmdSetNick(editName);
+                }
             }
+
+            GUILayout.BeginHorizontal();
+            for(int i = 0; i < songs.Length; i++)
+            {
+                if (GUILayout.Button("Song"+i))
+                {
+                    CmdSetSong(i);
+                }
+            }
+            GUILayout.EndHorizontal();
         }
     }
 
@@ -95,5 +123,18 @@ public class Walking : NetworkBehaviour {
     void CmdSetNick(string newName)
     {
         nick = newName;
+    }
+
+    [Command]
+    void CmdSetSong(int newSong)
+    {
+        song = newSong;
+    }
+
+    void OnSongChanged(int newSong)
+    {
+        song = newSong;
+        GetComponent<AudioSource>().clip = songs[newSong];
+        GetComponent<AudioSource>().Play();
     }
 }
